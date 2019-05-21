0c70a0e19820ac66b2d8586e9607e7d1e79e3f16
Zellat Abdelkhalek <ha_zellat@esi.dz>
2019-05-21 06:33:41 +0100

    Fix Gesture handler prblm

---
+++ android/app/src/main/java/com/tilawati/MainActivity.java
@@ -0,0 +1,34 @@
+package com.tilawati;
+
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+import org.devio.rn.splashscreen.SplashScreen; // here
+
+public class MainActivity extends ReactActivity {
+    // @Override
+    // protected void onCreate(Bundle savedInstanceState) {
+    //     SplashScreen.show(this); // here
+    //     super.onCreate(savedInstanceState);
+    // }
+
+    /**
+     * Returns the name of the main component registered from JavaScript. This is
+     * used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "Tilawati";
+    }
+
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+        return new ReactActivityDelegate(this, getMainComponentName()) {
+            @Override
+            protected ReactRootView createRootView() {
+                return new RNGestureHandlerEnabledRootView(MainActivity.this);
+            }
+        };
+    }
+}
