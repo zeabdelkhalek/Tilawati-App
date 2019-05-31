3ad479b144268b82a56fc8ac355aac9e5763fda8
Zellat Abdelkhalek <ha_zellat@esi.dz>
2019-05-31 09:50:25 +0100

    Fix screens header

--- App/Screens/SearchScreen.js
+++ App/Screens/SearchScreen.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import Header from '../Components/Header';
 
 class SearchScreen extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
 
-  render() {
-    return (
-      <View>
-        <Text> Search </Text>
-      </View>
-    );
-  }
+	render() {
+		return (
+			<View>
+				<Header />
+				<Text> Search </Text>
+			</View>
+		);
+	}
 }
 
 export default SearchScreen;
--- App/Screens/SettingsScreen.js
+++ App/Screens/SettingsScreen.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import Header from '../Components/Header';
 
 class SettingsScreen extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
 
-  render() {
-    return (
-      <View>
-        <Text> Settings </Text>
-      </View>
-    );
-  }
+	render() {
+		return (
+			<View>
+				<Header />
+				<Text> Settings </Text>
+			</View>
+		);
+	}
 }
 
 export default SettingsScreen;
