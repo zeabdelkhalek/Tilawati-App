a9fa84ea42e7ab7ca43750c70b99f5c328f99821
Zellat Abdelkhalek <ha_zellat@esi.dz>
2019-06-01 22:38:11 +0100

    add fonts assets & make app ready for production

--- android/app/src/main/java/com/tilawati/MainApplication.java
+++ android/app/src/main/java/com/tilawati/MainApplication.java
@@ -3,6 +3,7 @@ package com.tilawati;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+// import ui.fileselector.RNFileSelectorPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.zmxv.RNSound.RNSoundPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
       new MainReactPackage(),
+            // new RNFileSelectorPackage(),
             new ImagePickerPackage(),
             new RNSoundPackage(),
             new AsyncStoragePackage(),
